56f15ff375ec33e235f99693e076ac33ad59690d
indexpro24@gmail.com <indexpro24@gmail.com>
2022-04-10 19:36:07 +0300

    master

--- src/routes/web.php
+++ src/routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/health', function () {
+    return 'Hello World';
+});
 
 Route::get('/', function () {
     return view('welcome');
